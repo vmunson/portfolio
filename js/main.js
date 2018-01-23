5f078b10963e788e1fd965820123a838285ffa5c
vmunson <virginia.munson@gmail.com>
2018-01-23 09:26:01 -0500

    added formspree

--- js/main.js
+++ js/main.js
@@ -3,4 +3,7 @@ jQuery(document).ready(function ($) {
     // Initialize wowjs 
     new WOW().init();
 
+    $('#myModal').on('shown.bs.modal', function () {
+        $('#myInput').trigger('focus')
+      })
 });
