b88917d48e3de8a82906eff696a3e8b952136ae3
vmunson <virginia.munson@gmail.com>
2018-01-23 14:25:26 -0500

    added a scroll to top button

--- js/main.js
+++ js/main.js
@@ -3,5 +3,15 @@ jQuery(document).ready(function ($) {
     // Initialize wowjs 
     new WOW().init();
 
-    
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 50) {
+            $('#backToTop').fadeIn('slow');
+        } else {
+            $('#backToTop').fadeOut('slow');
+        }
+    });
+    $('#backToTop').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 500);
+        return false;
+    });
 });
