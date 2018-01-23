a4792820aabae4752276dd13636376ddee69fbe0
vmunson <virginia.munson@gmail.com>
2018-01-23 09:57:37 -0500

    added resume button

--- js/main.js
+++ js/main.js
@@ -3,7 +3,5 @@ jQuery(document).ready(function ($) {
     // Initialize wowjs 
     new WOW().init();
 
-    $('#myModal').on('shown.bs.modal', function () {
-        $('#myInput').trigger('focus')
-      })
+    
 });
